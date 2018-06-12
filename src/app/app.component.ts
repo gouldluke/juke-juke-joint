7bcf71e6b3890141dc85c95d6093f05e49f6ce8e
Luke Gould <lukegould@Lukes-MacBook-Pro.local>
2018-06-11 23:01:23 -0400

    first pass at the website

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+    eventbriteLink = "https://www.eventbrite.com/e/june-juke-joint-tickets-46057809147"
+    ampLink = "https://www.facebook.com/AmpNEO/"
+    blackMusicHistoryLink = "???";
+    facebookEventLink = "https://www.facebook.com/events/228436367921490/";
+    instagramLink = "https://www.instagram.com/ampbipocneo/";
+
+    adabLink = "https://soundcloud.com/adab/heaven-is-in-you-adab-wxcu-mix";
+    danteAndTayLink = "???";
+    fanaLink = "https://soundcloud.com/daisychain_podcast/daisychain-004-fana";
+    mazLink = "https://soundcloud.com/tendertones/sets/pale-moon";
+    shirtlessLink = "https://soundcloud.com/asiandopeboys/adbmix-shirtless-adb-misery-mix";
+    smokescreenLink = "https://soundcloud.com/smoke_screen/smoke-screen-ice-cold-water-01";
 }
